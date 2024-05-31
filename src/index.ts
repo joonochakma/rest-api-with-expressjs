0067d31b4bec54b0ae0af289577240b3c8a035e0
Joono Chakma <73777352+joonopohr@users.noreply.github.com>
2024-05-31 21:09:50 +1000

    add registration endpoint

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,8 @@ import cookieParser from 'cookie-parser';
 import compression from 'compression';
 import cors from 'cors';
 import mongoose from 'mongoose';
+import router from './router';
+
 const app = express();
 
 app.use(cors({
@@ -26,3 +28,5 @@ const MONGO_URL =
   mongoose.Promise = Promise;
   mongoose.connect(MONGO_URL);
   mongoose.connection.on('error', (error: Error) => console.log(error));
+
+  app.use('/', router());
