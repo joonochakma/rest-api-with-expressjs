c18735ae5fc9f3f8591ae73fbab84d60c762eba3
Joono Chakma <73777352+joonopohr@users.noreply.github.com>
2024-06-01 23:02:26 +1000

    add login controller

--- src/controllers/authentication.ts
+++ src/controllers/authentication.ts
@@ -3,6 +3,36 @@ import express from "express";
 import { getUserByEmail, createUser } from "../db/users";
 import { random, authentication } from "../helpers";
 
+export const login = async (req: express.Request, res: express.Response) => {
+    try {
+        const {email, password} = req.body;
+        if (!email || !password) {
+            return res.sendStatus(400);
+        }
+        const user = await getUserByEmail(email).select("+authentication.salt +authentication.password");
+
+        if (!user) {
+            return res.sendStatus(400);
+        }
+        //how authenticate without knowing password using hash comparison
+        const expectedHash = authentication(user.authentication.salt, password);
+
+        if (user.authentication.password != expectedHash) {
+            return res.sendStatus(403);
+        }
+    const salt = random();
+    user.authentication.sessionToken= authentication(salt, user.id.toString());
+    await user.save();
+
+    res.cookie('JOONO-AUTH', user.authentication.sessionToken, {domain: 'localhost', path: '/'})
+
+    return res.status(200).json(user).end();
+    } catch (error) {
+        console.log(error);
+        return res.sendStatus(400)
+    }
+}
+
 export const register = async (req: express.Request, res: express.Response) => {
   try {
     const { email, password, username } = req.body;
