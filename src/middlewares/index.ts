f2c51868b2e0199c8fb07e5e5a23ed1a99a224a9
Joono Chakma <73777352+joonopohr@users.noreply.github.com>
2024-06-04 00:20:39 +1000

    add isOwner, deleteUser, updateUser

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,16 +1,35 @@
 import express from 'express';
 import { get, merge } from 'lodash';
 
-import { getUserBySessionToken } from 'db/users';
+import { getUserBySessionToken } from '../db/users';
+
+export const isOwner = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
+try {
+const { id } = req.params;
+const currentUserId = get(req, 'identity._id') as string;
+
+if (!currentUserId ) {
+    return res.sendStatus(403);
+}
+
+if (currentUserId.toString() != id) {
+    return res.sendStatus(403);
+}
+next();
+}catch (error) {
+    console.log(error);
+    return res.sendStatus(400);
+}
+}
 
 export const isAuthenticated = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
     try {
-        const sessioToken = req.cookies['JOONO-AUTH'];
-        if (!sessioToken) {
+        const sessionToken = req.cookies['JOONO-AUTH'];
+        if (!sessionToken) {
             return res.sendStatus(403);
         }
 
-        const existingUser = await getUserBySessionToken(sessioToken);
+        const existingUser = await getUserBySessionToken(sessionToken);
 
         if (!existingUser) {
             return res.sendStatus(403);
--- src/router/users.ts
+++ src/router/users.ts
@@ -1,7 +1,13 @@
 import express from 'express';
 
-import { getAllUsers } from '../controllers/users';
+import { getAllUsers, deleteUser, updateUser } from "../controllers/users";
+import { isAuthenticated, isOwner } from '../middlewares';
+// isAuthenticated is the middleware validation 
+//isOwner restricts other users deleting one account
+//update api
 
 export default (router: express.Router) => {
-    router.get('/users', getAllUsers);
+    router.get('/users', isAuthenticated, getAllUsers);
+    router.delete('/users/:id',isAuthenticated, isOwner, deleteUser);
+    router.patch("/users/:id", isAuthenticated, isOwner, updateUser);
 };
